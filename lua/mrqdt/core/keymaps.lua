7ae7f555c6ea0a4a0143634be05d19b86761ae2b
jeppe <jepp026b@stud.kea.dk>
2024-05-31 21:44:37 +0200

    latest

--- lua/mrqdt/core/keymaps.lua
+++ lua/mrqdt/core/keymaps.lua
@@ -22,7 +22,7 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 -- window management
 keymap.set("n", "<C-w>io", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<C-w>op", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<C-w>vb", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<C-w>0", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 
 keymap.set("n", "<C-w>nm", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
@@ -31,4 +31,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
